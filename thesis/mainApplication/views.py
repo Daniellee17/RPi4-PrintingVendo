0a167a9103f9004a5920ee8d2e29d27cb0c7a82b
Daniellee17 <danielleespiritu17@gmail.com>
2021-01-30 16:38:28 +0800

    dd

--- thesis/mainApplication/views.py
+++ thesis/mainApplication/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+def mainPage(response):
+
+    if response.POST.get('action') == 'setup':
+
+        print(" ")
+        print("--------------------------- GrowSmart Initializing ----------------------------")
+        print(" ")
+        print(" ")
+
+        json = {
+        'modeNumber' : "1"
+        }
+        return JsonResponse(json)
+
+    if response.POST.get('action') == 'onFan':
+
+        print(" ")
+        print("~Air Circulation System Activated~")
+        print(" ")
+
+
+    return render(response, 'main.html')
